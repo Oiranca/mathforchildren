cd14d139480d69906e13a4ef069a8f30e5c4d009
Samuel Romero Arbelo <oiranca@gmail.com>
2023-03-04 00:56:02 +0000

    fix: container component

--- src/component/Container/Container.tsx
+++ src/component/Container/Container.tsx
@@ -2,7 +2,6 @@ import React, {useEffect, useState} from "react";
 import {MainMenu} from "../MainMenu/MainMenu";
 import {SubMenu} from "../SubMenu/SubMenu";
 import {Operation} from "../Operation/Operation";
-import correct from '../../img/correct.png'
 import "./Container.scss";
 
 // interface range {
@@ -11,9 +10,9 @@ import "./Container.scss";
 
 export const Container = () => {
 
-    const [qualify, setQualify] = useState<number>(0);
+    const [qualify, setQualify] = useState<number>(10);
     const [revise, setRevise] = useState<boolean>(true);
-    const [resultView, setResultView] = useState<string>('');
+    const [resultView, setResultView] = useState<string>('result-pass');
     const [digitSelected, setDigitSelected] = useState<number>(0);
     const [dataForExercise, setDataForExercise] = useState<Record<string, string | number>>({
         'firstNumberOperation': 0,
@@ -22,12 +21,11 @@ export const Container = () => {
     });
 
     useEffect(() => {
-        setResultView(qualify > 5 ? 'result-pass' : 'result-fail')
+        setResultView(qualify >= 5 ? 'result-pass' : 'result-fail')
     }, [qualify]);
 
     const calculateResult = (event: React.ChangeEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log('resultado')
     };
     const typeOperation = (type: string) => {
         setDataForExercise({...dataForExercise, typeOperation: type});
@@ -62,10 +60,8 @@ export const Container = () => {
                 <Operation digit={digitSelected}/>
                 <Operation digit={digitSelected}/>
                 <Operation digit={digitSelected}/>
-                <input type={"image"} className={'image-result'} alt={'type result'} src={correct}/>
                 <div className={'submit-result'}>
-
-                    <p id={resultView}>{revise ? 'Sin Corregir' : `${qualify}/10`}</p>
+                    <p id={resultView}>{revise ? 'Nota :' : `Nota : ${qualify}/10`}</p>
                     <button className={'button-result'} type={"submit"}>Corregir</button>
 
                 </div>
